b95df06302d996327c79aa2cfd3cf83a568006d6
user42 <kejebane@student.42.fr>
2022-10-29 23:57:20 +0200

    Progress saved, the php file is now correctly located and the config must be readjusted

--- Makefile
+++ Makefile
@@ -4,11 +4,17 @@ all:
 	docker-compose build
 	docker-compose up -d
 
+	#Build containers and run them in background
+
 down:
 	docker-compose down
+
+	#Stop all containers and remove their volumes
 rm:
 	docker volume rm $(docker volume ls -qf dangling=true | xargs)
 	docker volume rm $(docker volume ls -q)
+
+	#Remove all volumes of containers
 
 logdb:
 	docker logs --tail 50 --follow --timestamps mariadb_kejebane
@@ -17,11 +23,18 @@ logng:
 logwp:
 	docker logs --tail 50 --follow --timestamps wp_kejebane
 
+	#Display the error message that were not displayed because containers
+	#were running in background
+
 issue:
 	docker stop $(docker ps -a -q); docker rm $(docker ps -a -q); docker volume rm $(docker volume ls -qf dangling=true)
 	docker network rm $(docker network ls -q)
 	sudo lsof -nP | grep LISTEN
 	sudo kill -9 1548
+	
+	#Use if port 443 is occupied by another processor
 
 prune:
 	docker system prune
+
+	#Remove all dangling image, volume and network
--- wordpress/cmd.sh
+++ wordpress/cmd.sh
@@ -9,9 +9,25 @@ mv					-f wp-cli.phar /usr/local/bin/wp
 /usr/local/bin/wp	--info
 /usr/local/bin/wp	core download --allow-root --path="/var/www/html"
 
-rm					-f /var/www/html/wp-config.php
-cp					./wp-config.php /var/www/html/wp-config.php
+#rm					-f /var/www/html/wp-config.php
+#cp					./wp-config.php /var/www/html/wp-config.php
 
+echo					"P0"
+
+/usr/local/bin/wp	config create	--allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} \
+					--dbpass='user_pass'
+
+/bin/ls					/var/www/html/wp-config*
+#cp					./wp-config.php /var/www/html/wp-config.php
+#/bin/ls					/var/www/html/wp-config*
+
+echo					"P1"
+
+#/bin/cat				/var/www/html/wp-config-sample.php
+#/bin/cat				/var/www/html/wp-config.php
+#mv					/var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+echo					"P2"
 /usr/local/bin/wp	core install \
 					--allow-root \
 					--path="/var/www/html" \
@@ -22,6 +38,7 @@ cp					./wp-config.php /var/www/html/wp-config.php
 					--admin_email=${WORDPRESS_ADMIN_EMAIL} \
 					--skip-email
 
+echo					"P3"
 /usr/local/bin/wp	user create \
 					--allow-root \
 					--path="/var/www/html" \
@@ -29,5 +46,6 @@ cp					./wp-config.php /var/www/html/wp-config.php
 					${WORDPRESS_EMAIL} \
 					--role=author \
 					--user_pass=${WORDPRESS_PASSWORD}
+echo					"P4"
 
 exec	php-fpm7 -F
