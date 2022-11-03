2a02b31489282e275a9a18f133762ea0b5fd465c
user42 <kejebane@student.42.fr>
2022-11-03 19:46:20 +0100

    Cant connect to local MYSQL server through socket issue cant be solved, trying a differnet approach

--- mariadb/launch.sh
+++ mariadb/launch.sh
@@ -3,8 +3,10 @@
 mkdir -p /home/user42/data/database
 mkdir -p /home/user42/data/www
 
-/usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
+
+service mysql start
+#/usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
+#/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
@@ -12,5 +14,4 @@ mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 
-pkill mysqld
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
+#/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
