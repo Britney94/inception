4c943b076eba5cdcdc23ff7f44b56de0d9e5f7ae
Kevin JEBANE <kejebane@e3r7p5.clusters.42paris.fr>
2022-11-07 14:48:57 +0100

    Final modifications, using Debian buster now, completely reworked ig, Now that supposedly the Philosophy blog msg displays on localhost, what to do ?

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -7,14 +7,14 @@ define( 'DB_HOST', 'mariadb_kejebane' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-define('AUTH_KEY',         '2J/HrVAP*Qm>2-K:dO+3JRZ)c;*;;+Q](VaR<yMw%m%+I3ququj*RfFGe/ ?jhdq');
-define('SECURE_AUTH_KEY',  '](US01S5|5p>1LW9q:*$Vu+<Ky-{5;kq|e+vSnoqt>4w?HjO(++=5S>SA[Yo:z`L');
-define('LOGGED_IN_KEY',    'CLJ}R-PiGm|Zj15uyM {Ch!!8,rop*~Z4TafNMR6A%rw6|FYDLjj>o4,&= &D=3j');
-define('NONCE_KEY',        '*yrC0iMe+/*~CM2!x-m;v,rZ_s#HsFUt*AeYq3`?c>-*E<B^NlBUkr?AW2NHbOPX');
-define('AUTH_SALT',        '[A%RV%)(usmLa/#55~`zRPfxsRfJxh5@[LA{c;vji8Q;lp607Md}cgv+EAoY+5+|');
-define('SECURE_AUTH_SALT', 'ia$Q$oyduw`N~[+|_+s{,+t.mrb5#WsUIP@$[p6IQr86Zv(PBW6e2SSf|*G9D^[&');
-define('LOGGED_IN_SALT',   'Ym}rij`3+1F].pB|9.7}}+`<M[_a7.7e7^($]?RYRLT,3|&Bk4f>m$R*dny*+*W(');
-define('NONCE_SALT',       'sr-5R?tcH)%|kCB(gSM+ Fa(~mNG16A^/+@w.t u;qhW^;!T*f0Sk8,,RwC~|-j4');
+define('AUTH_KEY',         'X|_gQ`2-w &U$imehBii<w>O9YHCP{=h)DC*AzasN/:MN-HP/+3!b)O]GuiZY0?k');
+define('SECURE_AUTH_KEY',  'FR~HtTf-mVI]L`vr?6mKj[I<}2hoa#=Gg(Mp{!u$Gb.ySgR$s+_t3/AXXJ6OFM-z');
+define('LOGGED_IN_KEY',    '?E]`X/#`VM+Z~+3:AX&TG7x!K/$-CoMHe:#bWo:}e)RqF(A0^o+;)&Pt)Nkkeu>b');
+define('NONCE_KEY',        '$-~6+9m.mTR>HMh+d_T|8[ZnKM~EOpMawM-Z[[+=3GMIqlaOwc!|sufk^GRms]&+');
+define('AUTH_SALT',        'uHv`]v/fUp_#qeAe7K>T!OebnV6Jp=C$?QX-;ZvB:gv*5=KF~bPV|fehIc-u--|[');
+define('SECURE_AUTH_SALT', '>?C>I{u|Bo4U2k>L9AIIz+<;u+NF%4Q_,r>;~!!,TU3*+H%h9W@:oLTf?fJ_5UxT');
+define('LOGGED_IN_SALT',   'Mr4DL% %<k$$U%cX TXI0dR[Cp~`nG:4>Mf-fMFo>H>jMF~K,wvmA@Bm|E)Usp|v');
+define('NONCE_SALT',       '+=WyBG8E(@U.lco|Y+f-P#P!k#IN`a^+.,huU}+I{}vo%(c$$V{,8+^`~`SnGa|[');
 
 $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
