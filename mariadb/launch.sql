66f4bdc677f5b8c65b88877b1f8733041dcd5b00
user42 <kejebane@student.42.fr>
2022-11-09 12:31:39 +0100

    REWORKING AGAIN, LAST RESORT

--- mariadb/launch.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root_pass';
-CREATE DATABASE IF NOT EXISTS wpData;
-CREATE USER IF NOT EXISTS 'kejebane'@'%' IDENTIFIED BY 'kejebane_pass';
-CREATE USER IF NOT EXISTS 'brian'@'%' IDENTIFIED BY 'brian_pass';
-GRANT ALL PRIVILEGES ON wpData.* TO 'brian'@'%';
-GRANT ALL PRIVILEGES ON *.* TO 'kejebane'@'%';
-FLUSH PRIVILEGES;
