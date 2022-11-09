84f79850b62294b1afcd0d91fe9ceaf34922c2a0
user42 <kejebane@student.42.fr>
2022-11-09 15:33:25 +0100

    Root mysqld id is now properply stopepd when no pass, Ready for eval

--- srcs/requirements/mariadb/config/create_db.sql
+++ srcs/requirements/mariadb/config/create_db.sql
@@ -3,7 +3,7 @@ DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PWD');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PWD'
 
 CREATE DATABASE $MARIADB_DB;
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PWD';
